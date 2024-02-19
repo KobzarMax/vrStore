bc95318756435a2cb9081ebde649f27fc78a1fc7
Max Kobzar <nimbus2219@gmail.com>
2024-02-20 00:03:11 +0100

    slider styles

--- main.js
+++ main.js
@@ -130,7 +130,6 @@ document.addEventListener("DOMContentLoaded", () => {
     slidesPerView: 1,
     spaceBetween: 20,
     centeredSlides: true,
-    centeredSlides: true,
     breakpoints: {
       640: {
         slidesPerView: 2,
